6f59e4eba112d64dc95803a25ea19956e95724b9
Sean <51032338+Seanm64@users.noreply.github.com>
2023-11-09 12:03:11 -0500

    hotfix

--- testSet.py
+++ testSet.py
@@ -1,10 +1,11 @@
 class testSet():
 
     def __init__(self):
-        questionNumber = 0
+        self.__questionNumber = 0
         self.__testList = []
 
     def addQuestion(self, question):
+        self.__questionNumber += 1
         self.__testList.append(question)
 
     def getTestSet(self):
