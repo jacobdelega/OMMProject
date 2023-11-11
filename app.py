cb4f72aaae6f5a20609c1b477339e8bd7d8676f1
jacobdelega <82042739+jacobdelega@users.noreply.github.com>
2023-11-10 21:07:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -55,16 +55,107 @@ def login():
         #   Close Connection
 
         if user:
-            user_id = user[0]
-            username = user[1]
-            email = user[2]
-            session['user_id'] = user_id
-            session['username'] = username
-            session['email'] = email
-            return redirect(url_for('dashboard'))
+            users_id = user[0] 
+            user_firstName = user[2]
+            user_email = user[4]
+            session['users_id'] = users_id
+            session['user_firstName'] = user_firstName
+            session['user_email'] = user_email
+            print("Session data:", session)
+            return redirect(url_for('home'))
         else:
             flash("Invalid login", 'Invalid credentials')
+
+        #Close connection
+        dbConnect.close()
+        cursor.close()
     return render_template('index.html')
 
 
--- INSERT --                                                                                                                                                                                                             26,18         Top
+#   user auth complete, send them to home page
+@app.route('/home')
+@app.route('/home.html')
+def home():
+    firstName = session.get('user_firstName')
+    if firstName:
+        return render_template('home.html', firstName = firstName)
+    else:
+        flash("please log in to access the dashboard.", 'error')
+        return redirect(url_for('index.html'))
+    return render_template('home.html')
+
+@app.route('/dashboard')
+@app.route('/dashboard.html')
+def dashboard():
+
+    user_email = session.get('user_email')
+    if user_email:
+        return render_template('home.html', user_email=user_email)
+    else:
+        flash('Please log in to access the dashboard.', 'error')
+        return redirect(url_for('index'))
+
+import database_connection as dc
+#   User wants to go to create test page
+@app.route('/createTest', methods=['POST', 'GET'])
+@app.route('/createTest.html', methods=['POST', 'GET'])
+def createTest():
+    firstName = session.get('user_firstName') 
+    if request.method == 'POST':
+        
+        #Start connection
+        cnx = dc.makeConnection()
+        
+        isTutor = False
+        isTimed = False
+        if request.form.get("tutoredTest"):
+            isTutor = True
+        
+        if request.form.get("timedTest"):
+            isTimed = True
+        # TODO MAKE SURE TO CHANGE FORM VALUES FOR SELECTED_TAGS
+        # TODO TELL FRONTEND TO ADD IN MISSING TAGS
+
+        select_tags = request.form.getlist('tag_question') # contains list of tags
+        number_of_questions = int(request.form.get('numberInput')) # holds the number of questions student entered
+        users_id = session.get('users_id') # Grab current person logged in user_id
+
+        name_of_exam = "testv2"    #TODO Add in form to allow student to name exam?    
+
+        from make_test import makeTest
+        from datetime import date
+        date = str(date.today())
+
+        test_id = makeTest(cnx, select_tags, number_of_questions, users_id, name_of_exam, isTutor, isTimed, date)
+        return redirect(url_for('take_test', test_id=test_id))
+    return render_template('createTest.html', firstName=firstName)
+
+#   User is going to take test
+@app.route('/testTemp', methods = ['GET', 'POST'])
+@app.route('/testTemp.html', methods = ['GET', 'POST']) 
+def take_test():
+
+    test_id = request.args.get('test_id')
+    cnx = dc.makeConnection()
+
+    from get_test import getTest
+    testSet = getTest(cnx, test_id)
+    # print(testSet.getPrevQuestion().getQuestionText())
+
+    return render_template('testTemp.html', test_id=test_id, testList=testSet)
+
+#   User wants to go to view statistics
+@app.route('/viewStats')
+@app.route('/viewStats.html')
+def viewStats():
+    return render_template('viewStats.html')
+
+#   User wants to go to view tests
+@app.route('/viewTests')
+@app.route('/viewTests.html')
+def viewTests():
+    return render_template('viewTests.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True) 
