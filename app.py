dee98cff14cfbd5c6d2b18bad6e274be91fef110
Andy Tran <123677635+AndyT4@users.noreply.github.com>
2023-12-01 14:08:55 -0500

    Updated app.py from Jacob
    Added tag functionality and answers 5 and 6.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for, session,  flash
+from flask import Flask, render_template, request, redirect, url_for, session,  flash, jsonify
 import re
 import mysql.connector
 
@@ -42,6 +42,7 @@ def login():
 
     #   Grab user info
     if request.method == 'POST':
+        
         email = request.form['email']
         password = request.form['password']
         #   Start connection
@@ -55,10 +56,12 @@ def login():
         #   Close Connection
 
         if user:
-            users_id = user[0] 
+            users_id = user[0]
+            user_state = user[1] 
             user_firstName = user[2]
             user_email = user[4]
             session['users_id'] = users_id
+            session['user_state'] = user_state
             session['user_firstName'] = user_firstName
             session['user_email'] = user_email
             print("Session data:", session)
@@ -71,19 +74,92 @@ def login():
         cursor.close()
     return render_template('index.html')
 
+#   Adding a question
+@app.route('/addQuestion', methods=['GET', 'POST'])
+@app.route('/addQuestion.html', methods=['GET', 'POST'])
+def addQuestion():
+
+    if request.method == 'POST':
+
+
+        question_text = request.form['questionInput'] #"What is a human?"      # Temp value
+        example_text = request.form['explanationInput'] #"A human is a living being that walk on two legs."  #Temp value
+
+        # Get users id (faculty who created the question)
+        user_id = session.get('users_id')
+
+        # Start connection
+        cnx = dc.makeConnection()
+        cursor = cnx.cursor()
+
+        # Insert question text, example text, is_active (Default 1), users_id (Work in Progress)
+        insert_question = ("INSERT INTO question(question_text, example_text, is_active, users_ID) VALUES(%s, %s, %s, %s)")
+        values = (question_text, example_text, 1, user_id)
+        cursor.execute(insert_question, values)
+        cnx.commit()
+
+        # Get question id for question we just added
+        query_question = (f"SELECT question_ID FROM question WHERE question_text = \"{question_text}\"")
+        cursor.execute(query_question)
+        question_id = cursor.fetchall()[0][0]
+
+        selected_tags = request.form.getlist('subjectDropdown')
+
+        # For tags, loop through all the tags and see which one are checked
+        # Then query for the tag id and insert into tag_question before moving on to
+        # The next tag
+        for tag in selected_tags:
+            query_tag = (f"SELECT tag_ID FROM tag WHERE tag_name = \"{tag}\"")
+            cursor.execute(query_tag)
+            tag_id = cursor.fetchall()[0][0]
+
+            insert_tag_question = (f"INSERT INTO tag_question(tag_ID, question_ID) VALUES(%s, %s)")
+            values = (tag_id, question_id)
+            cursor.execute(insert_tag_question, values)
+            cnx.commit()
+
+        # insert_answer = (f"INSERT INTO answer(answer_text) VALUES(%s)")
+        answer_texts = [] # THIS IS FOR SPRINT MEETING TO SHOWCASE
+        for i in range(1, 6):
+            insert_answer = "INSERT INTO answer(answer_text) VALUES (%s)"
+            answer_text = request.form.get(f'answer{i}')
+            
+            if answer_text is not None:
+                is_correct = 1 if request.form.get(f'correctAnswer{i}') else 0
+                values = (answer_text,)
+                cursor.execute(insert_answer, values)
+                cnx.commit()
+                answer_texts.append(answer_text)
+            else:
+                print(f"Answer {i} is None.")
+
+            # Get answer id for the current answer 
+            query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer_text}\"")
+            cursor.execute(query_answer)
+            answer_id = cursor.fetchall()[0][0]
+
+            # Insert answer id into question_answer bridging table
+            insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
+            values = (question_id, answer_id, is_correct)
+            cursor.execute(insert_question_answer, values)
+            cnx.commit()
+
+
+        return redirect(url_for('success_page', question_text = question_text, question_id = question_id, answer_id = answer_id, is_correct=is_correct, answer_texts=answer_texts))
+    return render_template('addQuestion.html')
 
 #   user auth complete, send them to home page
 @app.route('/home')
 @app.route('/home.html')
 def home():
     firstName = session.get('user_firstName')
+    user_state = session.get('user_state')
     if firstName:
-        return render_template('home.html', firstName = firstName)
+        return render_template('home.html', firstName = firstName, user_state = user_state)
     else:
         flash("please log in to access the dashboard.", 'error')
         return redirect(url_for('index.html'))
     return render_template('home.html')
-
 
 @app.route('/dashboard')
 @app.route('/dashboard.html')
@@ -103,7 +179,6 @@ import database_connection as dc
 def createTest():
     firstName = session.get('user_firstName') 
     if request.method == 'POST':
-        
         #Start connection
         cnx = dc.makeConnection()
         
@@ -117,7 +192,7 @@ def createTest():
         # TODO MAKE SURE TO CHANGE FORM VALUES FOR SELECTED_TAGS
         # TODO TELL FRONTEND TO ADD IN MISSING TAGS
 
-        select_tags = request.form.getlist('tag_question') # contains list of tags
+        select_tags = request.form.getlist('category') # contains list of categories chosen
         number_of_questions = int(request.form.get('numberInput')) # holds the number of questions student entered
         users_id = session.get('users_id') # Grab current person logged in user_id
 
@@ -132,27 +207,20 @@ def createTest():
     return render_template('createTest.html', firstName=firstName)
 
 #   User is going to take test
-@app.route('/test', methods = ['GET', 'POST'])
-@app.route('/test.html', methods = ['GET', 'POST']) 
+@app.route('/testTemp', methods = ['GET', 'POST'])
+@app.route('/testTemp.html', methods = ['GET', 'POST']) 
 def take_test():
 
-    # checking if the answer is correct
-    msg = ""
-    if request.method == 'POST':
-        answerID = request.form['answerID']
-        if testSet.getCurrentQuestion().checkAnswer('answerID'):
-            msg = "Correct!"
-        elif testSet.getCurrentQuestion().checkAnswer('answerID') == False:
-            msg = "Sorry, incorrect."
-
     test_id = request.args.get('test_id')
+    
+    # TODO CHECK IF TEST_ID IS LINKED WITH USER_ID IF NOT THROW 404
+
     cnx = dc.makeConnection()
 
     from get_test import getTest
     testSet = getTest(cnx, test_id)
-    # print(testSet.getPrevQuestion().getQuestionText())
 
-    return render_template('test.html', test_id=test_id, testList=testSet)
+    return render_template('testTemp.html', test_id=test_id, testList=testSet)
 
 #   User wants to go to view statistics
 @app.route('/viewStats')
@@ -160,196 +228,35 @@ def take_test():
 def viewStats():
     return render_template('viewStats.html')
 
+@app.route('/success_page')
+@app.route('/success_page.html')
+def success_page():
+    question_id = request.args.get('question_id')
+    answer_id = request.args.get('answer_id')
+    is_correct = request.args.get('is_correct')
+    answer_texts = request.args.getlist('answer_texts')
+    question_text = request.args.get('question_text')
+    
+    return render_template('success_page.html', question_text = question_text, answer_texts=answer_texts, question_id = question_id, answer_id = answer_id, is_correct=is_correct)
+
 #   User wants to go to view tests
 @app.route('/viewTests')
 @app.route('/viewTests.html')
 def viewTests():
     return render_template('viewTests.html')
 
-#   Adding a question
-@app.route('/addQuestion')
-@app.route('/addQuestion.html')
-def addQuestion():
-    question_text = "This is a test for the tag?"      # Temp value
-    example_text = "Hey this is some example text."  #Temp value
-
-    # Get users id (faculty who created the question)
-    user_id = session.get('users_id')
-
-    # Start connection
-    cnx = dc.makeConnection()
-    cursor = cnx.cursor()
-
-    # Insert question text, example text, is_active (Default 1), users_id (Work in Progress)
-    insert_question = ("INSERT INTO question(question_text, example_text, is_active, users_ID) VALUES(%s, %s, %s, %s)")
-    values = (question_text, example_text, 1, user_id)
-    cursor.execute(insert_question, values)
-    cnx.commit()
-
-    # Get question id for question we just added
-    query_question = (f"SELECT question_ID FROM question WHERE question_text = \"{question_text}\"")
-    cursor.execute(query_question)
-    question_id = cursor.fetchall()[0][0]
+@app.route('/submit_data', methods=['POST'])
+def submit_data():
+    data = request.json
+    question_states = data.get('questionStates', [])
 
 
+    print(question_states)
+    # Process the received data as needed
+    # ...
 
-
-
-    # For tags, loop through all the tags and see which one are checked
-    # Then query for the tag id and insert into tag_question before moving on to
-    # The next tag
-
-    # This pseudocode only changes for a generic taglist but final version
-    # will include all the different tags (so 3 loops for each category)
-    """
-    for tag in taglist:
-        query_tag = (f"SELECT tag_ID FROM tag WHERE tag_name = \"{tag}\"")
-        cursor.execute(query_tag)
-        tag_id = cursor.fetchall()[0][0]
-
-        insert_tag_question = (f"INSERT INTO tag_question(tag_ID, question_ID) VALUES(%s, %s)")
-        values = (tag_id, question_id)
-        cursor.execute(insert_tag_question, values)
-        cnx.commit()
-    """
-
-    # Assume answers will be in their own textbox and each one will have 
-    # another radio button showing which one is correct
-    # check to see if the fifth answer is null (none)
-    answer1 = "1 for this question"
-    answer2 = "2 for this question"
-    answer3 = "3 for this question"
-    answer4 = "4 for this question"
-    is_Correct1 = 0
-    is_Correct2 = 0
-    is_Correct3 = 0
-    is_Correct4 = 1
-
-    # Insert answer1 into answer table
-    insert_answer = (f"INSERT INTO answer(answer_text) VALUES(%s)")
-    values = [(answer1)]
-    cursor.execute(insert_answer, values)
-    cnx.commit()
-
-    # Get answer id for answer1 
-    query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer1}\"")
-    cursor.execute(query_answer)
-    answer_id = cursor.fetchall()[0][0]
-
-    # Insert answer1 into question_answer bridging table
-    insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
-    values = (question_id, answer_id, is_Correct1)
-    cursor.execute(insert_question_answer, values)
-    cnx.commit()
-
-
-
-    # Insert answer2 into answer table
-    values = [(answer2)]
-    cursor.execute(insert_answer, values)
-    cnx.commit()
-
-    # Get answer id for answer2
-    query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer2}\"")
-    cursor.execute(query_answer)
-    answer_id = cursor.fetchall()[0][0]
-
-    # Insert answer2 into question_answer bridging table
-    insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
-    values = (question_id, answer_id, is_Correct2)
-    cursor.execute(insert_question_answer, values)
-    cnx.commit()
-
-
-
-    # Insert answer3 into answer table
-    values = [(answer3)]
-    cursor.execute(insert_answer, values)
-    cnx.commit()
-
-    # Get answer id for answer3
-    query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer3}\"")
-    cursor.execute(query_answer)
-    answer_id = cursor.fetchall()[0][0]
-
-    # Insert answer3 into question_answer bridging table
-    insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
-    values = (question_id, answer_id, is_Correct3)
-    cursor.execute(insert_question_answer, values)
-    cnx.commit()
-
-
-
-    # Insert answer4 into answer table
-    values = [(answer4)]
-    cursor.execute(insert_answer, values)
-    cnx.commit()
-
-    # Get answer id for answer4
-    query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer4}\"")
-    cursor.execute(query_answer)
-    answer_id = cursor.fetchall()[0][0]
-
-    # Insert answer4 into question_answer bridging table
-    insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
-    values = (question_id, answer_id, is_Correct4)
-    cursor.execute(insert_question_answer, values)
-    cnx.commit()
-
-
-
-    # Do insert answer 5 and 6
-    # answer_HTML is a placeholder for how we will get the information from the front
-    # is_Correct5 and is_Correct6 has a default value of 0 but that needs to get it
-    # from the front end as well
-    """
-    if (answer5HTML != ''):
-        answer5 = answer5HTML
-        is_Correct5 = 0
-
-        # Insert answer5 into answer table
-        values = [(answer5)]
-        cursor.execute(insert_answer, values)
-        cnx.commit()
-
-        # Get answer id for answer5
-        query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer5}\"")
-        cursor.execute(query_answer)
-        answer_id = cursor.fetchall()[0][0]
-
-        # Insert answer5 into question_answer bridging table
-        insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
-        values = (question_id, answer_id, is_Correct5)
-        cursor.execute(insert_question_answer, values)
-        cnx.commit()
-    """
-    """
-    if (answer6HTML != ''):
-        answer6 = answer6HTML
-        is_Correct6 = 0
-
-        # Insert answer6 into answer table
-        values = [(answer6)]
-        cursor.execute(insert_answer, values)
-        cnx.commit()
-
-        # Get answer id for answer6
-        query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer6}\"")
-        cursor.execute(query_answer)
-        answer_id = cursor.fetchall()[0][0]
-
-        # Insert answer6 into question_answer bridging table
-        insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
-        values = (question_id, answer_id, is_Correct6)
-        cursor.execute(insert_question_answer, values)
-        cnx.commit()
-    
-    """
-
-    cnx.Close()
-
-    
+    return jsonify({'message': 'Data received successfully'})
 
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, port=8000) 
