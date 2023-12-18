9535713629a4779ae74f5eeaffaa976f381488c8
atkins25 <127567949+atkins25@users.noreply.github.com>
2023-12-17 20:44:25 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template, request, redirect, url_for, session,  flash, jsonify
 import re
 import mysql.connector
+import Answer, Question
+from werkzeug.utils import secure_filename
+
+# Directory where images are stored
+UPLOAD_FOLDER = 'C:\OMM\question_images'
 
 app = Flask(__name__)
 app.secret_key = 'verySecretKey'
@@ -149,10 +154,89 @@ def addQuestion():
         return redirect(url_for('success_page', question_text = question_text, question_id = question_id, answer_id = answer_id, is_correct=is_correct, answer_texts=answer_texts))
     return render_template('addQuestion.html')
 
+
+
+#   Adding a question
+@app.route('/editQuestion', methods=['GET', 'POST'])
+@app.route('/editQuestion.html', methods=['GET', 'POST'])
+def editQuestion():
+
+    if request.method == 'POST':
+
+
+        question_text = request.form['questionInput'] #"What is a human?"      # Temp value
+        example_text = request.form['explanationInput'] #"A human is a living being that walk on two legs."  #Temp value
+
+        # Get users id (faculty who created the question)
+        user_id = session.get('users_id')
+
+        # Start connection
+        cnx = dc.makeConnection()
+        cursor = cnx.cursor()
+
+        # Insert question text, example text, is_active (Default 1), users_id (Work in Progress)
+        insert_question = ("INSERT INTO question(question_text, example_text, is_active, users_ID) VALUES(%s, %s, %s, %s)")
+        values = (question_text, example_text, 1, user_id)
+        cursor.execute(insert_question, values)
+        cnx.commit()
+
+        # Get question id for question we just added
+        query_question = (f"SELECT question_ID FROM question WHERE question_text = \"{question_text}\"")
+        cursor.execute(query_question)
+        question_id = cursor.fetchall()[0][0]
+
+        selected_tags = request.form.getlist('subjectDropdown')
+        
+        # For tags, loop through all the tags and see which one are checked
+        # Then query for the tag id and insert into tag_question before moving on to
+        # The next tag
+        for tag in selected_tags:
+            query_tag = (f"SELECT tag_ID FROM tag WHERE tag_name = \"{tag}\"")
+            cursor.execute(query_tag)
+            tag_id = cursor.fetchall()[0][0]
+
+            insert_tag_question = (f"INSERT INTO tag_question(tag_ID, question_ID) VALUES(%s, %s)")
+            values = (tag_id, question_id)
+            cursor.execute(insert_tag_question, values)
+            cnx.commit()
+
+        answer_texts = [] # THIS IS FOR SPRINT MEETING TO SHOWCASE
+        for i in range(1, 6):
+            insert_answer = "INSERT INTO answer(answer_text) VALUES (%s)"
+            answer_text = request.form.get(f'answer{i}')
+
+            if answer_text is not None:
+
+                is_correct = 1 if request.form.get(f'correctAnswer{i}') else 0
+                values = (answer_text,)
+                cursor.execute(insert_answer, values)
+                cnx.commit()
+                answer_texts.append(answer_text)
+
+                # Get answer id for answer1 
+                query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer_text}\"")
+                cursor.execute(query_answer)
+                answer_id = cursor.fetchall()[0][0]
+
+                # Insert answer id into question_answer bridging table
+                insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
+                values = (question_id, answer_id, is_correct)
+                cursor.execute(insert_question_answer, values)
+                cnx.commit()
+            else:
+                print(f"Answer {i} is None.")
+
+
+
+        return redirect(url_for('success_page', question_text = question_text, question_id = question_id, answer_id = answer_id, is_correct=is_correct, answer_texts=answer_texts))
+    return render_template('editQuestion.html')
+
+
 @app.route('/searchQuestion')
 @app.route('/searchQuestion.html')
 def searchQuestion():
     return render_template('searchQuestion.html')
+
 
 #   user auth complete, send them to home page
 @app.route('/home')
@@ -281,7 +365,6 @@ def submit_data():
 
     print(question_states)
     return jsonify({'message': 'Data received successfully'})
-
 
 if __name__ == '__main__':
     app.run(debug=True, port=8000) 
