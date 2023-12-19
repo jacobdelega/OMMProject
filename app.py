12d262276aea9137c6f291053a2133775808f4af
Ricky Krame <CorgiCluster@gmail.com>
2023-12-19 02:39:38 -0500

    Finished view attempts

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask, render_template, request, redirect, url_for, session,  flash
+from flask import Flask, render_template, request, redirect, url_for, session,  flash, jsonify
 import re
 import mysql.connector
+import os
+from werkzeug.utils import secure_filename
+import get_attempts
 
 app = Flask(__name__)
 app.secret_key = 'verySecretKey'
@@ -42,6 +45,7 @@ def login():
 
     #   Grab user info
     if request.method == 'POST':
+        
         email = request.form['email']
         password = request.form['password']
         #   Start connection
@@ -55,10 +59,12 @@ def login():
         #   Close Connection
 
         if user:
-            users_id = user[0] 
+            users_id = user[0]
+            user_state = user[1] 
             user_firstName = user[2]
             user_email = user[4]
             session['users_id'] = users_id
+            session['user_state'] = user_state
             session['user_firstName'] = user_firstName
             session['user_email'] = user_email
             print("Session data:", session)
@@ -71,14 +77,89 @@ def login():
         cursor.close()
     return render_template('index.html')
 
+#   Adding a question
+@app.route('/addQuestion', methods=['GET', 'POST'])
+@app.route('/addQuestion.html', methods=['GET', 'POST'])
+def addQuestion():
+
+    if request.method == 'POST':
+
+
+        question_text = request.form['questionInput'] #"What is a human?"      # Temp value
+        example_text = request.form['explanationInput'] #"A human is a living being that walk on two legs."  #Temp value
+
+        # Get users id (faculty who created the question)
+        user_id = session.get('users_id')
+
+        # Start connection
+        cnx = dc.makeConnection()
+        cursor = cnx.cursor()
+
+        # Insert question text, example text, is_active (Default 1), users_id (Work in Progress)
+        insert_question = ("INSERT INTO question(question_text, example_text, is_active, users_ID) VALUES(%s, %s, %s, %s)")
+        values = (question_text, example_text, 1, user_id)
+        cursor.execute(insert_question, values)
+        cnx.commit()
+
+        # Get question id for question we just added
+        query_question = (f"SELECT question_ID FROM question WHERE question_text = \"{question_text}\"")
+        cursor.execute(query_question)
+        question_id = cursor.fetchall()[0][0]
+
+        selected_tags = request.form.getlist('subjectDropdown')
+        
+        # For tags, loop through all the tags and see which one are checked
+        # Then query for the tag id and insert into tag_question before moving on to
+        # The next tag
+        for tag in selected_tags:
+            query_tag = (f"SELECT tag_ID FROM tag WHERE tag_name = \"{tag}\"")
+            cursor.execute(query_tag)
+            tag_id = cursor.fetchall()[0][0]
+
+            insert_tag_question = (f"INSERT INTO tag_question(tag_ID, question_ID) VALUES(%s, %s)")
+            values = (tag_id, question_id)
+            cursor.execute(insert_tag_question, values)
+            cnx.commit()
+
+        answer_texts = [] # THIS IS FOR SPRINT MEETING TO SHOWCASE
+        for i in range(1, 6):
+            insert_answer = "INSERT INTO answer(answer_text) VALUES (%s)"
+            answer_text = request.form.get(f'answer{i}')
+
+            if answer_text is not None:
+
+                is_correct = 1 if request.form.get(f'correctAnswer{i}') else 0
+                values = (answer_text,)
+                cursor.execute(insert_answer, values)
+                cnx.commit()
+                answer_texts.append(answer_text)
+
+                # Get answer id for answer1 
+                query_answer = (f"SELECT answer_ID FROM answer WHERE answer_text = \"{answer_text}\"")
+                cursor.execute(query_answer)
+                answer_id = cursor.fetchall()[0][0]
+
+                # Insert answer id into question_answer bridging table
+                insert_question_answer = ("INSERT INTO question_answer(question_ID, answer_ID, is_correct) VALUES(%s, %s, %s)")
+                values = (question_id, answer_id, is_correct)
+                cursor.execute(insert_question_answer, values)
+                cnx.commit()
+            else:
+                print(f"Answer {i} is None.")
+
+
+
+        return redirect(url_for('success_page', question_text = question_text, question_id = question_id, answer_id = answer_id, is_correct=is_correct, answer_texts=answer_texts))
+    return render_template('addQuestion.html')
 
 #   user auth complete, send them to home page
 @app.route('/home')
 @app.route('/home.html')
 def home():
     firstName = session.get('user_firstName')
+    user_state = session.get('user_state')
     if firstName:
-        return render_template('home.html', firstName = firstName)
+        return render_template('home.html', firstName = firstName, user_state = user_state)
     else:
         flash("please log in to access the dashboard.", 'error')
         return redirect(url_for('index.html'))
@@ -102,7 +183,6 @@ import database_connection as dc
 def createTest():
     firstName = session.get('user_firstName') 
     if request.method == 'POST':
-        
         #Start connection
         cnx = dc.makeConnection()
         
@@ -115,8 +195,8 @@ def createTest():
             isTimed = True
         # TODO MAKE SURE TO CHANGE FORM VALUES FOR SELECTED_TAGS
         # TODO TELL FRONTEND TO ADD IN MISSING TAGS
-
-        select_tags = request.form.getlist('tag_question') # contains list of tags
+        
+        select_tags = request.form.getlist('category') # contains list of categories chosen
         number_of_questions = int(request.form.get('numberInput')) # holds the number of questions student entered
         users_id = session.get('users_id') # Grab current person logged in user_id
 
@@ -124,10 +204,16 @@ def createTest():
 
         from make_test import makeTest
         from datetime import date
+        from make_attempt import makeAttempt
         date = str(date.today())
 
         test_id = makeTest(cnx, select_tags, number_of_questions, users_id, name_of_exam, isTutor, isTimed, date)
-        return redirect(url_for('take_test', test_id=test_id))
+        attempt_num, attempt_id = makeAttempt(cnx, test_id)
+        session['test_id'] = test_id
+        session['attempt_num'] = attempt_num
+        session['attempt_id'] = attempt_id
+        # print(f"Attempt ID: {attempt_id}, Attempt Number: {attempt_num}")
+        return redirect(url_for('take_test', test_id=test_id, isTimed=isTimed, isTutor=isTutor))
     return render_template('createTest.html', firstName=firstName)
 
 #   User is going to take test
@@ -136,13 +222,16 @@ def createTest():
 def take_test():
 
     test_id = request.args.get('test_id')
+    isTimed = request.args.get('isTimed')
+    isTutor = request.args.get('isTutor')
+    # TODO CHECK IF TEST_ID IS LINKED WITH USER_ID IF NOT THROW 404
+
     cnx = dc.makeConnection()
 
     from get_test import getTest
     testSet = getTest(cnx, test_id)
-    # print(testSet.getPrevQuestion().getQuestionText())
 
-    return render_template('testTemp.html', test_id=test_id, testList=testSet)
+    return render_template('testTemp.html', test_id=test_id, testList=testSet, isTimed=isTimed, isTutor=isTutor)
 
 #   User wants to go to view statistics
 @app.route('/viewStats')
@@ -150,12 +239,100 @@ def take_test():
 def viewStats():
     return render_template('viewStats.html')
 
+@app.route('/success_page')
+@app.route('/success_page.html')
+def success_page():
+    question_id = request.args.get('question_id')
+    answer_id = request.args.get('answer_id')
+    is_correct = request.args.get('is_correct')
+    answer_texts = request.args.getlist('answer_texts')
+    question_text = request.args.get('question_text')
+    
+    return render_template('success_page.html', question_text = question_text, answer_texts=answer_texts, question_id = question_id, answer_id = answer_id, is_correct=is_correct)
+
 #   User wants to go to view tests
 @app.route('/viewTests')
 @app.route('/viewTests.html')
 def viewTests():
-    return render_template('viewTests.html')
+
+    user_id = session.get('users_id')
+    cnx = dc.makeConnection()
+
+    attempts = get_attempts.getAttempts(cnx, user_id)
+    
+    return render_template('viewTests.html', attempts = attempts)
+
+@app.route('/viewAttempt/<test_id>/<attempt_num>')
+@app.route('/viewAttempt.html')
+def viewAttempt(test_id, attempt_num):
+
+    cnx = dc.makeConnection()
+
+    from get_test import getTest
+    import get_answer
+    testSet = getTest(cnx, test_id)
+
+    for question in testSet.getTestSet():
+        question_id = question.getID()
+        question.setGivenAnswer(get_answer.getAnswer(cnx, test_id, attempt_num, question_id))
+
+    cnx.close()
+
+    return render_template('viewAttempt.html', test_id=test_id, testList=testSet)
+
+import datetime 
+
+@app.route('/testResult')
+@app.route('/testResult.html')
+def testResult():
+    examTime = session.get('exam_time')
+    score = session.get('score')
+    question_states = session.get('question_states')
+
+    #Format for better output on results page. 
+    format_time = datetime.timedelta(seconds=examTime)
+    return render_template('testResult.html', examTime=format_time, score=score, question_states=question_states)
+
+@app.route('/submit_data', methods=['POST'])
+def submit_data():
+
+    data = request.json
+    question_states = data.get('questionStates', [])
+
+    session['question_states'] = question_states
+
+    last_question = question_states[-1] # Only needed to grab the score and the time
+    if 'time' in last_question:
+        session['exam_time'] = last_question['time']
+    
+    if 'score' in last_question:
+        session['score'] = last_question['score']
+
+    from insert_answer import insertAnswer
+    for state in question_states:
+        # print(state)
+        selected_answer = state.get('selectedAnswer') #Good
+        question_id = state.get('questionID') #Good
+        isCorrect = state.get('feedback')#Good, int val
+        test_id = session.get('test_id')#Good, int val
+        attempt_number = session.get('attempt_num') #Good, int val
+
+        #Insert answer into database
+        cnx = dc.makeConnection()
+        insertAnswer(cnx, test_id, attempt_number, question_id, selected_answer, isCorrect)
+
+    #Update attempt when exam is complete
+    attempt_id = session.get('attempt_id')
+    score = session.get('score')
+    is_complete = 1
+    time_taken = session.get('exam_time')   
+
+    from update_attempt import updateAttempt
+    cnx = dc.makeConnection()   
+    updateAttempt(cnx, attempt_id, score, is_complete, time_taken)
+
+    return jsonify({'message': 'Data received successfully'})
 
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=True, port=8000) 
---
+++ config.py
@@ -0,0 +1,7 @@
+
+class config():
+
+    database_username = 'kramer13'
+    database_pass = 'Anikawolf637'
+    database_host = "3.87.120.222"
+    database = 'omm'
