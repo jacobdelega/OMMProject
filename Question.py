8bdf4747be60b01c3e6c1fa87d877b6426e90bd1
jacobdelega <82042739+jacobdelega@users.noreply.github.com>
2023-12-20 10:38:37 -0500

    Update Question.py

--- Question.py
+++ Question.py
@@ -1,13 +1,30 @@
 import Answer
+import base64
 
 class Question:
 
-    #constructor
+    # upload folder
+    UPLOAD_FOLDER = 'static/question_images'
+
+    #constructor with images
+    def __init__(self, questionID, questionText, exampleText, answers, image, explanationImage):
+        self.questionText = questionText 
+        self.questionID = questionID  
+        self.exampleText = exampleText                 
+        self.answers = answers
+        self.image = image
+        self.explanationImage = explanationImage
+        self.givenAnswer = None
+
+    #constructor without images
     def __init__(self, questionID, questionText, exampleText, answers):
         self.questionText = questionText 
         self.questionID = questionID  
         self.exampleText = exampleText                 
         self.answers = answers
+        self.image = None
+        self.explanationImage = None
+        self.givenAnswer = None
 
     #setters
     def setQuestionText(self, questionText):
@@ -16,21 +33,20 @@ class Question:
     def setID(self, ID):
         self.ID = ID
 
-    # Duplicate    
-    # def setAnswers(self, answers):
-    #     self.answers = answers
-
     def setExampleText(self, exampleText):
         self.exampleText = exampleText
 
     def setAnswers(self, answers):
-        self.answersSQL = answers.split(", ")
-        self.answers = []
-        index = 0
-        for i in self.answersSQL:
-            self.answersSQL[index] = i.strip("[]") 
-            self.answers.append(Answer.Answer(self.answersSQL[index].split(":")[0], self.answersSQL[index].split(":")[1]))
-            index += 1
+        self.answers = answers
+
+    def setImage(self, image):
+        self.image = image
+
+    def setExplanationImage(self, image):
+        self.explanationImage = image
+
+    def setGivenAnswer(self, answer_id):
+        self.givenAnswer = answer_id
 
     #getters
     def getQuestionText(self):
@@ -44,3 +60,24 @@ class Question:
     
     def getExampleText(self):
         return self.exampleText
+
+    def getImage(self):
+        if (self.image is not None):
+            pathToImage = self.UPLOAD_FOLDER + "/" + str(self.image)
+
+            with open(pathToImage, "rb") as image_file:
+                encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+            return encoded_string
+        return None
+
+    def getExplanationImage(self):
+        if (self.explanationImage is not None):
+            pathToImage = self.UPLOAD_FOLDER + "/" + str(self.explanationImage)
+
+            with open(pathToImage, "rb") as explanation_image_file:
+                encoded_string = base64.b64encode(explanation_image_file.read()).decode("utf-8")
+            return encoded_string
+        return None
+    
+    def getGivenAnswer(self):
+        return self.givenAnswer
